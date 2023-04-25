37f94afa8f4ca6f84fabf913c26a49e59fb6f30c
Zander <114107976+Femnyx@users.noreply.github.com>
2023-04-25 13:16:48 -0500

    Properties is done the thing!

---
+++ Properties_Mcvey_Zander/Cars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Mcvey_Zander
+{
+    class Cars
+    {
+
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+
+
+
+    }
+}
--- Properties_Mcvey_Zander/Program.cs
+++ Properties_Mcvey_Zander/Program.cs
@@ -6,7 +6,21 @@ namespace Properties_Mcvey_Zander
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Cars car = new Cars();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            Cars car2 = new Cars();
+            car2.Make = "Chevy";
+            car2.Model = "Camero";
+
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            Cars car3 = new Cars();
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
+           
         }
     }
 }
